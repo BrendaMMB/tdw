cd184e66955db9d51e181863741872eb44cc1a62
BrendaMMB <brendammenezes@gmail.com>
2025-05-02 16:31:02 -0300

    atualizando credenciais

--- app-firebase-auth/src/firebase.js
+++ app-firebase-auth/src/firebase.js
@@ -15,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+console.log('API_KEY:', process.env.REACT_APP_FIREBASE_API_KEY);
